47b5f1660f74ecfd53aa0c3d88bd93818cf89326
ramironinni <ramironinni@hotmail.com>
2020-11-01 21:15:13 -0300

    Creación de proyecto NPM con Express y estructura de archivos

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/productDetail.html'));
+});
+
+app.get('/productCart', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/productCart.html'));
+});
