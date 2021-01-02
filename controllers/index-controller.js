645e6a471d4696c4cf7505ba6fd821a024688e06
ramironinni <ramironinni@hotmail.com>
2021-01-02 16:29:47 -0300

    Se agrega funcionalidad a la barra de búsquedas

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,11 +1,35 @@
 const getProducts = require("../utils/getProducts");
-const productsList = getProducts();
 const toThousand = require("../utils/toThousand");
 
 const indexController = {
     show: (req, res) => {
+        const productsList = getProducts();
         res.render("index", {
             products: productsList,
+            moneda: "ARS",
+            toThousand,
+        });
+    },
+    search: (req, res) => {
+        const products = getProducts();
+
+        const searchString = req.query.search;
+
+        const searchArray = searchString.split(" ");
+
+        let foundProducts = [];
+        searchArray.forEach((word) => {
+            products.forEach((product) => {
+                if (
+                    product.name.toLowerCase().indexOf(word.toLowerCase()) > -1
+                ) {
+                    foundProducts.push(product);
+                }
+            });
+        });
+        // res.send(foundProducts);
+        res.render("index", {
+            products: foundProducts,
             moneda: "ARS",
             toThousand,
         });
