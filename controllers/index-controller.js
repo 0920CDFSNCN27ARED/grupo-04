36fe00be6401564b3017d437b3e3f42d765c7a27
ramironinni <ramironinni@hotmail.com>
2021-01-02 17:06:25 -0300

    Solucionado el problema de search que arrojaba datos duplicados cuando varias palabras matcheaban

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -20,14 +20,17 @@ const indexController = {
         let foundProducts = [];
         searchArray.forEach((word) => {
             products.forEach((product) => {
-                if (
-                    product.name.toLowerCase().indexOf(word.toLowerCase()) > -1
-                ) {
-                    foundProducts.push(product);
+                if (product.name.toLowerCase().includes(word.toLowerCase())) {
+                    const alreadyFound = foundProducts.find(
+                        (foundProduct) => foundProduct.id == product.id
+                    );
+                    if (!alreadyFound) {
+                        foundProducts.push(product);
+                    }
                 }
             });
         });
-        // res.send(foundProducts);
+
         res.render("index", {
             products: foundProducts,
             moneda: "ARS",
